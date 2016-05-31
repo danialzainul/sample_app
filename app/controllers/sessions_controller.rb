82d34168b683cdcd474f119acb491a4f8d9dd708
Danial <danialzainul@hotmail.com>
2016-05-31 19:20:47 +0800

    completed users authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
     	log_in user
       # this means if remember_me == 1, remember. else forget
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-    	redirect_to user
+    	redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'

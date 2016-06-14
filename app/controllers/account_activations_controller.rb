4cf8b1462efc314e31f8e6aa02dc6464a49375a5
Danial <danialzainul@hotmail.com>
2016-06-14 17:42:15 +0800

    WIP user activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,7 +3,9 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.activate
+      byebug
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
@@ -12,5 +14,4 @@ class AccountActivationsController < ApplicationController
       redirect_to root_url
     end
   end
-  
 end

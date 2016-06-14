7a6b5ae7ec66288caf772962c67e0dd0920e6249
Danial <danialzainul@hotmail.com>
2016-06-14 14:44:43 +0800

    added user activation migration

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
 
   resources :users
+  resources :account_activations, only: [:edit]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
